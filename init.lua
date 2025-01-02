7e56358790fe07c05f86f3bbb7d47aae19fe0205
Yussif Karim <karimyussif63@gmail.com>
2025-01-02 13:55:19 +0000

    minor fixes

--- init.lua
+++ init.lua
@@ -713,7 +713,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { "prettierd", "prettier", stop_after_first = true },
-        html ={"prettierd", "prettier"}
+        html ={"prettierd", "prettier"},
         css = {"prettierd", "prettier"}
       },
     },
