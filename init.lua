736d734be6ed50cf12d3af516b97707cce5cb9d6
Yussif Karim <karimyussif63@gmail.com>
2025-03-02 23:24:51 +0000

    add some personal preference

--- init.lua
+++ init.lua
@@ -658,7 +658,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
+        ruff ={},
+        ruff_lsp ={},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -759,6 +761,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
+        python = { "ruff" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
